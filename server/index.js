dd4e758b7a24d91050c0e166cf4aff25ec807814
Taylor <taybelle2020@gmail.com>
2025-04-09 18:23:42 -0400

    only deployment is left

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(logRoutes);
 const path = require("path");
 
 // the filepath is to the entire assets folder
-const filepath = path.join(__dirname, "../vite-project/dist");
+const filepath = path.join(__dirname, "../frontend/dist");
 
 // generate middleware using the filepath
 const serveStatic = express.static(filepath);
@@ -23,26 +23,44 @@ const serveStatic = express.static(filepath);
 // Register the serveStatic middleware before the remaining controllers
 app.use(serveStatic);
 
-// other controllers
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // controllers
+//Picture
 const servePicture = (req, res) => {
   // console.log(req.query);
-  res.send(
-    `https://cdn.britannica.com/19/213119-050-C81C786D/Grumpy-Cat-2015-memes.jpg`
-  );
+  res.send({
+    src: `https://cdn.britannica.com/19/213119-050-C81C786D/Grumpy-Cat-2015-memes.jpg`,
+  });
 };
+//Joke
 const serveJoke = (req, res) => {
-  res.json({
+  // res.json({
+  //   setup: "What happens to an illegally parked frog?",
+  //   punchline: "It gets toad away.",
+  // });
+  res.send({
     setup: "What happens to an illegally parked frog?",
     punchline: "It gets toad away.",
   });
 };
-const serveRollDie = (req, res) => {
-  const { num } = req.query;
-  if (!num) {
-    return res.send("hi");
+//Roll the Dice
+const serveRollDice = (req, res) => {
+  // const { num } = req.query;
+  // if (!num) {
+  //   return res.send("hi");
+  // }
+  // const  quantity = req.query.quantity
+  // req.query = {
+  //   quantity: 3
+  // }
+  const { quantity } = req.query;
+  const length = Number(quantity) > 0 ? Number(quantity) : 1;
+  const rolls = [];
+  for (let i = 0; i < length; i++) {
+    const roll = Math.ceil(Math.random() * 6);
+    rolls.push(roll);
   }
+  res.send(rolls);
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -53,7 +71,7 @@ app.get("/api/picture", servePicture);
 app.get("/api/joke", serveJoke);
 
 // // endpoint 3
-app.get("/api/rollDice", serveRollDie);
+app.get("/api/rollDie", serveRollDice);
 
 // port
 const PORT = 8080;
